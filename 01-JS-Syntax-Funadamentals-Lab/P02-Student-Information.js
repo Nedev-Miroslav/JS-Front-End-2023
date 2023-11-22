bee2c058b2a2f1aa699243a054117ce6b0ada091
Miroslav Nedev <117746535+Nedev-Miroslav@users.noreply.github.com>
2023-11-22 10:59:16 +0200

    JS-Syntax Funadamentals Lab

---
+++ 01-JS-Syntax-Funadamentals-Lab/P02-Student-Information.js
@@ -0,0 +1,10 @@
+function solve(name, age, grade){
+    console.log(`Name: ${name}, Age: ${age}, Grade: ${grade.toFixed(2)}`)
+}
+
+
+
+solve("Peter", 16, 5.666666)
+solve("Peter", 16, 5.866666)
+
+
