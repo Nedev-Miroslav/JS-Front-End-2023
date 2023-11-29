2572ddd1724b369c5e94697fba7ee96054522031
Miroslav Nedev <117746535+Nedev-Miroslav@users.noreply.github.com>
2023-11-29 09:34:28 +0200

    JS Arrays and Strings Lab

---
+++ 03-JS-Arrays-And-Strings-Lab/P02DifferentSolutionReverseAnArrayOfNumbers.js
@@ -0,0 +1,12 @@
+function reverseArrayOfNumbers(index, input){
+    let subArr = input.slice(0, index);
+    subArr.reverse()
+    let result = "";
+    for (let currentNum of subArr) {
+        result += `${currentNum} `;
+    }
+    console.log(result);
+}
+
+
+reverseArrayOfNumbers(4, [-1, 20, 99, 5]);
