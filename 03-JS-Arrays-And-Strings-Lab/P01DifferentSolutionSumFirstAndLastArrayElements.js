869e97e6510abaf30fc72aa1d467bf539b6251c6
Miroslav Nedev <117746535+Nedev-Miroslav@users.noreply.github.com>
2023-11-29 09:33:26 +0200

    JS Arrays and Strings Lab

---
+++ 03-JS-Arrays-And-Strings-Lab/P01DifferentSolutionSumFirstAndLastArrayElements.js
@@ -0,0 +1,10 @@
+function sumOfFirstAndLastElement(input) {
+    let firstNum = input[0];
+    let lastNum = input[input.length - 1];
+    console.log(firstNum + lastNum);
+
+}
+
+
+
+sumOfFirstAndLastElement([20, 30, 40]);
