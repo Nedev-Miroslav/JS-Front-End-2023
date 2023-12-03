06efdeb94d0b3b1d442027a975656123562b3f24
Miroslav Nedev <117746535+Nedev-Miroslav@users.noreply.github.com>
2023-12-03 11:50:23 +0200

    JS Arrays and Strings Exercise

---
+++ 04-JS-Arrays-And-Strings-Exercise/P03List0fNames.js
@@ -0,0 +1,12 @@
+function listOfNames(input){
+
+    input.sort((a, b) => {
+       return a.localeCompare(b);     
+    });
+
+    for(let i = 0; i < input.length; i++){
+        console.log(`${i + 1}.${input[i]}`);
+    }
+}
+
+listOfNames(["John", "Bob", "Christina", "Ema"])
