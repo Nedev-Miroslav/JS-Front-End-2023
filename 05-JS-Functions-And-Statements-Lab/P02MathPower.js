7eeb6cc5ef54c6f806e0d36cadd3a4dcad7e5459
Miroslav Nedev <117746535+Nedev-Miroslav@users.noreply.github.com>
2023-12-05 11:15:48 +0200

    JS Functions and Statements Lab

---
+++ 05-JS-Functions-And-Statements-Lab/P02MathPower.js
@@ -0,0 +1,16 @@
+function mathPower(number, power){
+    console.log(Math.pow(number, power));
+}
+
+
+// Друго решение на задачата
+// function mathPower(number, power) {
+//     let result = number;
+//     for (let i = 1; i < power; i++) {
+//         result *= number;
+//     }
+//     console.log(result);
+// }
+
+
+mathPower(2, 8);
