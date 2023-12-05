95a75f60f91822fe2591c0f823361e7ced52eeaf
Miroslav Nedev <117746535+Nedev-Miroslav@users.noreply.github.com>
2023-12-05 11:15:36 +0200

    JS Functions and Statements Lab

---
+++ 05-JS-Functions-And-Statements-Lab/P02DifferentSolutionMathPower.js
@@ -0,0 +1,10 @@
+function mathPower(number, power) {
+    let result = number;
+    for (let i = 1; i < power; i++) {
+        result *= number;
+    }
+    console.log(result);
+}
+
+
+mathPower(2, 8);
