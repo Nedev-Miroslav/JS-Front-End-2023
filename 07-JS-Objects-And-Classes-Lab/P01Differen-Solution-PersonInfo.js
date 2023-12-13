4af00d3357800f551c5ba63afb5c3606768ca2c9
Miroslav Nedev <117746535+Nedev-Miroslav@users.noreply.github.com>
2023-12-13 17:42:30 +0200

    JS Objects and Classes-Lab

---
+++ 07-JS-Objects-And-Classes-Lab/P01Differen-Solution-PersonInfo.js
@@ -0,0 +1,11 @@
+function personInfo(firstName, lastName, age){
+
+    return {
+        firstName: firstName,
+        lastName: lastName,
+        age: age,
+        
+    };
+}
+
+console.log(personInfo("Peter", "Pan", "20"));
