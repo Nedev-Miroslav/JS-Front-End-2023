6571af2d5d4d8e237f591fd29914b53df913478f
Miroslav Nedev <117746535+Nedev-Miroslav@users.noreply.github.com>
2023-12-22 13:13:50 +0200

    JS Objects and Classes Exercise

---
+++ 08-JS-Objects-And-Classes-Exercise/P04Movies.js
@@ -0,0 +1,154 @@
+function checkMovie(input) {
+
+    const movies = [];
+
+    input.forEach((element) => {
+
+        if (element.includes("addMovie")) {
+
+            let movie = element.split("addMovie ")[1];
+            movies.push({ name: movie });
+
+        } else if (element.includes("directedBy")) {
+
+            let [movie, director] = element.split(" directedBy ");
+
+            let searching = movies.find((e) => e.name === movie);
+
+            if (searching) {
+                searching["director"] = director;
+            }
+
+        } else if (element.includes("onDate")) {
+            let [movie, date] = element.split(" onDate ");
+
+            let searching = movies.find((e) => e.name === movie);
+
+            if (searching) {
+                searching["date"] = date;
+            }
+        }
+
+    });
+
+    movies.forEach((movie) => {
+        if (movie.name && movie.date && movie.director) {
+            console.log(JSON.stringify(movie));
+
+        }
+    });
+}
+
+checkMovie([
+    'addMovie Fast and Furious',
+    'addMovie Godfather',
+    'Inception directedBy Christopher Nolan',
+    'Godfather directedBy Francis Ford Coppola',
+    'Godfather onDate 29.07.2018',
+    'Fast and Furious onDate 30.07.2018',
+    'Batman onDate 01.08.2018',
+    'Fast and Furious directedBy Rob Cohen'
+]);
+
+
+
+
+
+// Друго решение на задачата
+// function solve(input) {
+
+//     class MovieLibrary {
+//         constructor() {
+//             this.movies = [];
+//         }
+
+//         addMovie(command) {
+//             const [_, name] = command.split("addMovie ");
+//             this.movies.push({ name });
+//         }
+
+//         addDirector(command) {
+//             const [movieName, directorName] = command.split(" directedBy ");
+//             const movie = this.movies.find((m) => m.name === movieName);
+
+//             if (movie) {
+//                 movie.director = directorName;
+//             }
+//         }
+
+//         addDate(command) {
+//             const [movieName, date] = command.split(" onDate ");
+//             const movie = this.movies.find((m) => m.name === movieName);
+
+//             if (movie) {
+//                 movie.date = date;
+//             }
+//         }
+//     }
+
+//     function createMovieLibrary() {
+//         let movies = [];
+
+//         return {
+//             addMovie: (command) => {
+//                 const [_, name] = command.split("addMovie ");
+//                 movies.push({ name });
+//             },
+//             addDirector: (command) => {
+//                 const [movieName, directorName] = command.split(" directedBy ");
+//                 const movie = movies.find((m) => m.name === movieName);
+
+//                 if (movie) {
+//                     movie.director = directorName;
+//                 }
+//             },
+//             addDate: (command) => {
+//                 const [movieName, date] = command.split(" onDate ");
+//                 const movie = movies.find((m) => m.name === movieName);
+
+//                 if (movie) {
+//                     movie.date = date;
+//                 }
+//             },
+//             getMovies() {
+//                 return movies;
+//             },
+//         };
+//     }
+
+//     const movieLibrary = new MovieLibrary();
+
+//     input.forEach((command) => {
+//         if (command.includes("addMovie")) {
+//             movieLibrary.addMovie(command);
+//         } else if (command.includes("directedBy")) {
+//             movieLibrary.addDirector(command);
+//         } else if (command.includes("onDate")) {
+//             movieLibrary.addDate(command);
+//         }
+//     });
+
+//     movieLibrary.movies
+//         .filter((m) => m.name && m.director && m.date)
+//         .forEach((m) => console.log(JSON.stringify(m)));
+// }
+
+// solve([
+//     "addMovie Fast and Furious",
+//     "addMovie Godfather",
+//     "Inception directedBy Christopher Nolan",
+//     "Godfather directedBy Francis Ford Coppola",
+//     "Godfather onDate 29.07.2018",
+//     "Fast and Furious onDate 30.07.2018",
+//     "Batman onDate 01.08.2018",
+//     "Fast and Furious directedBy Rob Cohen",
+// ]);
+
+// solve([
+//     "addMovie The Avengers",
+//     "addMovie Superman",
+//     "The Avengers directedBy Anthony Russo",
+//     "The Avengers onDate 30.07.2010",
+//     "Captain America onDate 30.07.2010",
+//     "Captain America directedBy Joe Russo",
+// ]);
