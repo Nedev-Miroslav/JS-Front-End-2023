9166f9fc8b4dca8b20d5c2c6cd00c6d9f5b25c7c
Miroslav Nedev <117746535+Nedev-Miroslav@users.noreply.github.com>
2023-12-29 12:21:36 +0200

    JS-Front-End-DOM-Introduction-Lab

---
+++ 09-JS-Front-End-DOM-Introduction-Lab/04.Show More/ShowMore.js
@@ -0,0 +1,8 @@
+function showText() {
+    const elementToHide = document.getElementById("more");
+    elementToHide.style.display = "none";
+    
+    const moreText = document.getElementById("text");
+    moreText.style.display = "inline";
+
+}
