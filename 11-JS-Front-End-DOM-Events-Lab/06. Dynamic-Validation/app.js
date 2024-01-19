46066f405784c4f82cf8e6a17049357b5b1ef892
Miroslav Nedev <117746535+Nedev-Miroslav@users.noreply.github.com>
2024-01-19 12:31:10 +0200

    JS-Front-End DOM Events Lab

---
+++ 11-JS-Front-End-DOM-Events-Lab/06. Dynamic-Validation/app.js
@@ -0,0 +1,15 @@
+function validate() {
+    document.getElementById('email').addEventListener("change", onChange);
+
+    function onChange (e){
+        const element = e.currentTarget;
+        let pattern = /[a-z]+@[a-z]+\.[a-z]+/gm
+
+        if(!pattern.test(element.value)){
+            element.classList.add("error");
+        } else{
+            element.classList.remove("error");
+        }
+    }
+    
+}
