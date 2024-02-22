78fe465b3e083e293d5fa1c58004c9ad890ef76b
Miroslav Nedev <117746535+Nedev-Miroslav@users.noreply.github.com>
2024-02-22 10:50:08 +0200

    More tasks

---
+++ More tasks/number-modification.js
@@ -0,0 +1,26 @@
+function solve(num) {
+
+function calculateAverage(num) {
+    return (
+      num
+        .split("")
+        .map(Number)
+        .reduce((total, curr) => total + curr, 0) / num.length
+    );
+  }
+
+
+
+    num = num.toString();
+    let average = calculateAverage(num);
+  
+    while (average < 5) {
+      num += "9";
+      average = calculateAverage(num);
+    }
+  
+    return num;
+  }
+  
+  console.log(solve(101));
+  console.log(solve(5835));
